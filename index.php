d1ea3bdb305bdd65aefb26d9fa853b1a9730d7f5
jfarcher <jf_archer@yahoo.com>
2014-09-08 14:24:48 +0100

    Changes to login screen layout and design.

--- index.php
+++ index.php
@@ -3,69 +3,45 @@ session_start();
 if(!isset($_SESSION["ticketsuser"])){
 
 $page=$_GET["page"];
-
-print ("
-
+?>
+<!DOCTYPE html>
 <html>
-<head>
-<link rel=\"stylesheet\" type=\"text/css\" media=\"screen\" href=\"style.css\">
-</head>
-<body >
-	<form id=\"login-form\" action=\"login.php\" method=\"post\">
-		<fieldset >
-		
-			<legend>Log in</legend>
-			
-			<label  for=\"login\">Email</label>
-			<input  type=\"text\" id=\"ticketsuser\" name=\"ticketsuser\"/>
-			<div class=\"clear\"></div>
-			
-			<label for=\"password\">Password</label>
-			<input type=\"password\" id=\"ticketspass\" name=\"ticketspass\"/>
-			<div class=\"clear\"></div>
-			
-			<label for=\"remember_me\" style=\"padding: 0;\">Remember me?</label>
-			<input type=\"checkbox\" id=\"remember_me\" style=\"position: relative; top: 3px; margin: 5; \" name=\"remember_me\"/>
-			<div class=\"clear\"></div>
-			
-			<br />
-			
-			<input type=\"submit\" style=\"margin: -20px 0 0 287px;\" class=\"button\" name=\"commit\" value=\"Log in\"/>	
-		</fieldset>
-	</form>
 
-<!--<table width=\"300\" border=\"0\" align=\"center\" cellpadding=\"0\" cellspacing=\"1\" bgcolor=\"#CCCCCC\">
-<tr>
-<form name=\"form 1\" method=\"post\" action=\"login.php\">
-<td>
-<table width=\"100%\" border=\"0\" cellpadding=\"3\" cellspacing=\"1\" bgcolor=\"#FFFFFF\">
-<tr>
-<td colspan=\"3\"><strong>Login</strong></td>
-</tr>
-<tr>
-<td width=\"78\">Username</td>
-<td width=\"6\">:</td>
-<td width=\"294\"><input name=\"ticketsuser\" type=\"text\" id=\"ticketsuser\"></td>
-</tr>
-<tr>
-<td>Password</td>
-<td>:</td>
-<td><input name=\"ticketspass\" type=\"password\" id=\"ticketspass\"></td>
-</tr>
-<tr>
-<td>&nbsp;</td>
-<td>&nbsp;</td>
-<input type=\"hidden\" name=\"page\" value=\"$page\">
-<td><input type=\"submit\" name=\"Submit\" value=\"Login\"></td>
-</tr>
-</table>
-</td>
-</form>
-</tr>
-</table>-->
-</div>
-");
+<head>
+
+  <meta charset="UTF-8">
+
+  <title>Tickets</title>
+<link rel="stylesheet" type="text/css" media="screen" href="style-login.css">
+
+    <script src="libs/prefixfree.min.js"></script>
+
+</head>
+
+<body>
+
+  <div class="body"></div>
+		<div class="grad"></div>
+		<div class="header">
+			<div>Tickets</div>
+		</div>
+		<br>
+		<div class="login">
+				<form id="login-form" action="login.php" method="post">
+				<input type="text" placeholder="Username" name="ticketsuser"><br>
+				<input type="password" placeholder="Password" name="ticketspass"><br>
+				<input type="submit" value="Login">
+			</form>
+		</div>
+
+  <script src='http://codepen.io/assets/libs/fullpage/jquery.js'></script>
+
+</body>
+
+</html>
+<?php
 }
 else{
 header("location:admin.php");
 }
+
